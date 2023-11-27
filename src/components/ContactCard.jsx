0f2fc92dbc474e276b5399b597c590f88ae4e699
anu03k <kafleanuja1@gmail.com>
2023-11-27 19:03:35 +0545

    DeleteContact

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,10 +1,27 @@
+import { deleteDoc, doc } from 'firebase/firestore'
 import React from 'react'
 import { FaUser, FaUserEdit } from 'react-icons/fa'
 import { IoMdTrash } from 'react-icons/io'
-
+import { db } from '../config/firebase';
 
 // props
 const ContactCard = ({contact}) => {
+
+    // delete contact
+    const deleteContact= async (id)=>{
+      try{
+        await deleteDoc(doc(db, "contacts" , id));
+
+      }catch(error){
+        console.log(error)
+      }
+    }
+
+
+
+
+
+
   return <>
    <div key={contact.id} className=' flex bg-powdered_blue justify-between items-center p-2 rounded-lg '>
          
@@ -20,7 +37,7 @@ const ContactCard = ({contact}) => {
 
            <div className='flex text-3xl '>
            <FaUserEdit className='text-black cursor-pointer' />
-           <IoMdTrash className='text-gray cursor-pointer'/>
+           <IoMdTrash className='text-gray cursor-pointer'   onClick={()=> deleteContact(contact.id)}/>
          
 
            </div>
